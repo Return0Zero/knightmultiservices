0b96d9d0913a9aec6820c792f3b7b2d5ea60d1f4
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-07-07 06:31:53 -0400

    header2

--- knight/src/containers/header/header.js
+++ knight/src/containers/header/header.js
@@ -4,14 +4,17 @@ import CleaningCrew from '../../images/vectors/cleaning-crew.svg'
 function Header() {
   return (
     <div className="header-container">
-      <div className="header-text">
-        <h1 className="header-h1" >We are the solution to all your cleaning and maintenance problems</h1>
-        <p className="header-p">Outstanding Landscaping, Commercial and Residential Cleaning Services in the DMV area.</p>
-        <button className="header-button">Contact Us</button>
+      <div className="header-content">
+        <div className="header-text">
+          <h1 className="header-h1" >We are the solution to all your cleaning and maintenance problems</h1>
+          <p className="header-p">Outstanding Landscaping, Commercial and Residential Cleaning Services in the DMV area.</p>
+          <button className="header-button">Contact Us Now!</button>
+        </div>
+        <div>
+          <img src={CleaningCrew} alt="Cleaning Crew" className="cleaningcrew"/>
+        </div>
       </div>
-      <div>
-        <img src={CleaningCrew} alt="Cleaning Crew" className="cleaningcrew"/>
-      </div>
+      
     </div>
   );
 }
