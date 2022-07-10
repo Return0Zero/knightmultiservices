f5175226eda0d30adaa8591f5be367133d0c2d7e
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-07-10 09:14:18 -0400

    header

--- knight/src/containers/header/header.js
+++ knight/src/containers/header/header.js
@@ -1,9 +1,20 @@
 import './header.css';
-import CleaningCrew from '../../images/vectors/cleaning-crew.svg'
+import CleaningCrew from '../../images/vectors/cleaning-crew.svg';
+import ThinStar from '../../images/icons/star1.svg';
+import ThickStar from '../../images/icons/star2.svg';
+import OrangeTopDesign from '../../images/vectors/orange-top-design.svg';
 
 function Header() {
   return (
     <div className="header-container">
+    <img src={ThickStar} alt="Thick Star" className="star thick-star1"/>
+    <img src={ThickStar} alt="Thick Star" className="star thick-star2"/>
+    <img src={ThickStar} alt="Thick Star" className="star thick-star3"/>
+    <img src={ThickStar} alt="Thick Star" className="star thick-star4"/>
+    <img src={ThinStar} alt="Thin Star" className="star thin-star1"/>
+    <img src={ThinStar} alt="Thin Star" className="star thin-star2"/>
+    <img src={ThinStar} alt="Thin Star" className="star thin-star3"/>
+    <img src={ThinStar} alt="Thin Star" className="star thin-star4"/>
       <div className="header-content">
         <div className="header-text">
           <h1 className="header-h1" >We are the solution to all your cleaning and maintenance problems</h1>
@@ -15,6 +26,7 @@ function Header() {
         </div>
       </div>
       
+    <img src={OrangeTopDesign} alt="Orange Top Design" className="orange-top-design"/>
     </div>
   );
 }
